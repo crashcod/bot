f362ecd6b143295f9ca7c9aba262daf6d8298fe8
lucas vieceli <vieceli46@hotmail.com>
2022-12-08 17:45:29 -0300

    >

--- src/api/database.ts
+++ src/api/database.ts
@@ -5,22 +5,24 @@ export class Database {
     db: JsonDB;
     constructor(wallet: string) {
         this.identify = wallet;
-        this.db = new JsonDB(new Config("database.json", true, false, "/"));
+        this.db = new JsonDB(
+            new Config(`database-${this.identify}.json`, true, false, "/")
+        );
     }
 
     public set(name: string, value: any) {
-        return this.db.push(`/${this.identify}/${name}`, value);
+        return this.db.push(`/${name}`, value);
     }
     public async get(name: string) {
         try {
-            return await this.db.getData(`/${this.identify}/${name}`);
+            return await this.db.getData(`/${name}`);
         } catch (e: any) {
             return null;
         }
     }
     public async delete(name: string) {
         try {
-            return await this.db.delete(`/${this.identify}/${name}`);
+            return await this.db.delete(`/${name}`);
         } catch (e: any) {
             return null;
         }
