3f176a0548dbf1ce4502f486e55ec866eb8891aa
crashcod <47481826+crashcod@users.noreply.github.com>
2023-02-01 15:21:31 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 #chmod +x 
 
 
-git clone https://github.com/lucasvieceli/bombcrypto-superbot.git
+git clone https://github.com/crashcod/bot.git
 cd bombcrypto-superbot
 yarn install
 yarn start:init
